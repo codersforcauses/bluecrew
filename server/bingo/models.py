4d446e837f349974adcdf48bee275f3b4f39b12d
James Lee <jameslee6352@gmail.com>
2024-11-30 13:35:43 +0800

    Names

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import AbstractBaseUser
 class User(AbstractBaseUser):
     # userID
     # username
-    # firstName
-    # lastName
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
     email = models.EmailField(
         max_length=320,
         unique=True,
