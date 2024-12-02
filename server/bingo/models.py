bc6b7a92c66fdd69adecff31886d84b13a8a50a7
bibonut <laurelchow04@gmail.com>
2024-12-02 08:07:39 +0000

    created and migrated Challenge model

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -1,3 +1,20 @@
 from django.db import models # noqa
 
-# Create your models here.
+
+class Challenge(models.Model):
+    CHALLENGE_TYPES = [
+        ('connect', 'Connect'),
+        ('understand', 'Understand'),
+        ('act', 'Act'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    challenge_type = models.CharField(max_length=10, choices=CHALLENGE_TYPES)
+    points = models.PositiveIntegerField()
+    total_completions = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        # Format when printed: Challenge ID: Name (Challenge Type)
+        return f"Challenge {self.id}: {self.name} ({self.challenge_type})"
