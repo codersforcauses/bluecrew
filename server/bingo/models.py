88e01fd7dddc1cd5fe7e5b1b0b3c66eaa14cb0e0
SafetyInObscurity <jameslee6352@gmail.com>
2024-12-07 05:12:18 +0000

    Fixing a few fields

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.base_user import AbstractBaseUser
 
 
 class User(AbstractBaseUser):
-    REQUIRED_FIELDS = ["username", "firstName", "lastName", "birthdate", "email", "visibility"]
+    REQUIRED_FIELDS = ["username", "firstName", "lastName", "birthdate", "email", "visibility", "avatar"]
 
     userID = models.BigAutoField(primary_key=True)
 
     username = models.CharField(max_length=30, unique=True)
     USERNAME_FIELD = "username"
 
-    firstName = models.CharField(max_length=30)
-    lastName = models.CharField(max_length=30)
+    firstName = models.CharField(max_length=30, blank=False)
+    lastName = models.CharField(max_length=30, blank=False)
     bio = models.CharField(max_length=300)
     totalPoints = models.IntegerField(default=0)
-    birthdate = models.DateField()
+    birthdate = models.DateField(blank=False)
 
     email = models.EmailField(
         max_length=320,
@@ -28,9 +28,10 @@ class User(AbstractBaseUser):
         FRIENDS = (1, "Friends Only")
         BLUECREW = (0, "BlueCrew Only")
     visibility = models.IntegerField(
-        choices=Visibility
+        choices=Visibility,
+        blank=False
     )
-    avatar = models.AutoField()
+    avatar = models.IntegerField()
 
     is_active = models.BooleanField(default=True)
 
