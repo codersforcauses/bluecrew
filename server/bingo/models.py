e956a7d4351afe37bead2b89e5150285f55b36c0
bibonut <laurelchow04@gmail.com>
2024-12-07 04:51:08 +0000

    removed id field

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -8,7 +8,6 @@ class Challenge(models.Model):
         ('act', 'Act'),
     ]
 
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True)
     challenge_type = models.CharField(max_length=10, choices=CHALLENGE_TYPES)
@@ -17,4 +16,4 @@ class Challenge(models.Model):
 
     def __str__(self):
         # Format when printed: Challenge ID: Name (Challenge Type)
-        return f"Challenge {self.id}: {self.name} ({self.challenge_type})"
+        return f"Challenge {self.id}: {self.name} ({self.challenge_type.capitalize()})"
