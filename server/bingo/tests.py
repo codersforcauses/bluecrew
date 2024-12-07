b5c8be1b43185378791fc315c630571c052350df
bibonut <laurelchow04@gmail.com>
2024-12-07 04:51:46 +0000

    django unit tests

--- server/bingo/tests.py
+++ server/bingo/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase # noqa
+from django.test import TestCase
+from .models import Challenge
 
-# Create your tests here.
+
+class ChallengeTest(TestCase):
+    def setUp(self):
+        Challenge.objects.create(
+            name="Test",
+            description="This is a test challenge.",
+            challenge_type="act",
+            points=5,
+            total_completions=10,
+        )
+
+    def test_create_challenge(self):
+        challenge = Challenge.objects.get(name="Test")
+        self.assertEqual(challenge.description, "This is a test challenge.")
+        self.assertEqual(challenge.challenge_type, "act")
+        self.assertEqual(challenge.points, 5)
+        self.assertEqual(challenge.total_completions, 10)
+        self.assertEqual(str(challenge), "Challenge 1: Test (Act)")
