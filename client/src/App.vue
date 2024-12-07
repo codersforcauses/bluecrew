268b93f90f038e5841051280613ab5a277d46095
aradavi63 <adam.ahmadavi@gmail.com>
2024-12-07 05:04:48 +0000

    cleaning up

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,9 +7,12 @@ const dialog = ref(false)
 
 <template>
   <div class="pa-4 text-center">
-    <v-dialog max-width="30%" v-model="dialog" scrollable>
+    <v-dialog max-width="30%" v-model="dialog" scrollable persistent>
       <template v-slot:activator="{ props: activatorProps }">
-        <v-btn id="button" v-bind="activatorProps" text="Registration"></v-btn>
+        <v-btn 
+        class="bg-primaryBlue"
+        v-bind="activatorProps" 
+        text="Registration"></v-btn>
       </template>
 
       <v-card>
@@ -23,14 +26,4 @@ const dialog = ref(false)
 </template>
 
 <style scoped>
-#button {
-  width: 100%;
-  padding: 12px;
-  border: none;
-  border-radius: 25px;
-  cursor: pointer;
-  font-size: 16px;
-  margin-top: 10px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.16);
-}
 </style>
