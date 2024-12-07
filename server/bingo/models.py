a7c128ceabc417f24b018962ec204979ed33568c
James Lee <jameslee6352@gmail.com>
2024-12-07 14:28:24 +0800

    Adding user manager

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -1,9 +1,35 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, firstName, lastName, birthdate, password=None):
+
+        user = self.model(
+            self.normalize_email(email),
+            firstName=firstName,
+            lastName=lastName,
+            birthdate=birthdate
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+    def create_superuser(self, email, firstName, lastName, birthdate, password=None):
+        user = self.model(
+            self.normalize_email(email),
+            firstName=firstName,
+            lastName=lastName,
+            birthdate=birthdate
+        )
+        user.is_admin = True
+        user.set_password(password)
+        return user
 
 
 class User(AbstractBaseUser):
-    REQUIRED_FIELDS = ["firstName", "lastName", "birthdate", "email", "visibility", "avatar"]
+    REQUIRED_FIELDS = ["firstName", "lastName", "birthdate", "email"]
 
     userID = models.BigAutoField(primary_key=True)
 
@@ -29,11 +55,15 @@ class User(AbstractBaseUser):
         BLUECREW = (0, "BlueCrew Only")
     visibility = models.IntegerField(
         choices=Visibility,
-        blank=False
+        blank=False,
+        default=0
     )
-    avatar = models.IntegerField()
+    avatar = models.IntegerField(default=0)
 
     is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     def __str__(self):
         return self.username
