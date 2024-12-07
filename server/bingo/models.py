37b81680730d28ed02188a2e6bcea3a18ef25247
James Lee <jameslee6352@gmail.com>
2024-12-07 14:45:44 +0800

    Minor fixes

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, firstName, lastName, birthdate, password=None):
+    def create_user(self, username, email, firstName, lastName, birthdate, password=None):
 
         user = self.model(
-            self.normalize_email(email),
+            username,
+            email=self.normalize_email(email),
             firstName=firstName,
             lastName=lastName,
             birthdate=birthdate
@@ -15,16 +16,18 @@ class UserManager(BaseUserManager):
 
         user.set_password(password)
         user.save(using=self.db)
+        return user
 
-    def create_superuser(self, email, firstName, lastName, birthdate, password=None):
-        user = self.model(
-            self.normalize_email(email),
+    def create_superuser(self, username, email, firstName, lastName, birthdate, password=None):
+        user = self.create_user(
+            username,
+            email=email,
             firstName=firstName,
             lastName=lastName,
             birthdate=birthdate
         )
         user.is_admin = True
-        user.set_password(password)
+        user.save(using=self._db)
         return user
 
 
@@ -64,6 +67,10 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
     def __str__(self):
         return self.username
