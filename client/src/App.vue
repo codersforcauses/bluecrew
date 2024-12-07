c4a0716266c4f9c43615027b58e697e949933306
yuwengchoong <yuweng.choong@gmail.com>
2024-12-07 04:23:45 +0000

    imported component to app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import LoginModal from './components/LoginModal.vue'
 </script>
 
 <template>
   <RouterView />
+  <LoginModal />
 </template>
 
 <style></style>
