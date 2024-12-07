8a8bf10964f8cb63e3efa2759546d7fed7ade0c9
自助餐 <zzcnhy@sina.com>
2024-12-07 12:47:09 +0800

    page

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,28 @@
 import { RouterView } from 'vue-router'
 import RegisterModal from './components/RegisterModal.vue'
 import { ref } from 'vue'
-
-const isOpen = ref(false)
+const dialog = ref(false)
 </script>
 
 <template>
+  <div class="pa-4 text-center">
+    <v-dialog max-width="30%" v-model="dialog" scrollable>
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-btn id="button" v-bind="activatorProps" text="Registration"></v-btn>
+      </template>
+
+      <v-card>
+        <v-card-text style="height: auto; overflow-y: auto">
+          <RegisterModal @close="dialog = false" />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
   <RouterView />
-  <button @click="isOpen = true">Register</button>
-  <RegisterModal :open="'isOpen'" @close="isOpen = !isOpen"></RegisterModal>
 </template>
 
 <style scoped>
-button {
+#button {
   width: 100%;
   padding: 12px;
   border: none;
