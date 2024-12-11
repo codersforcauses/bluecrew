412de9720e6da9f1a846dfd0757d1f73cd2b88b8
James Lee <jameslee6352@gmail.com>
2024-12-11 18:56:32 +0800

    Fixes

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -1,35 +1,37 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email, password, **extra_fields):
 
         user = self.model(
             username=username,
-            email=self.normalize_email(email)
+            email=self.normalize_email(email),
+            **extra_fields
         )
 
         user.set_password(password)
         user.save(using=self.db)
         return user
 
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, username, email, password, **extra_fields):
         user = self.create_user(
             username=username,
             email=email,
-            password=password
+            password=password,
+            **extra_fields
         )
-        user.is_admin = True
+        user.is_superuser = True
         user.save(using=self.db)
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ["email"]
 
-    user_id = models.BigAutoField(primary_key=True)
+    user_id = models.AutoField(primary_key=True)
 
     username = models.CharField(max_length=30, unique=True)
     USERNAME_FIELD = "username"
@@ -56,22 +58,16 @@ class User(AbstractBaseUser):
         blank=False,
         default=0
     )
-    avatar = models.IntegerField(default=0)
+
+    avatar = models.IntegerField(default=0, choices=map(lambda i: (i, f"Avatar {i}"), range(6)))
 
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
     @property
     def is_staff(self):
-        return self.is_admin
+        return self.is_superuser
 
     def __str__(self):
         return self.username
