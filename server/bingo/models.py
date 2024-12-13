9f8bc8925adf38868b00ed184f8aae7e50205d88
Laurel <126889403+bibonut@users.noreply.github.com>
2024-12-13 22:51:38 +0800

    Issue 16 create challenge db model (#30)
    * created and migrated Challenge model
    
    * removed id field
    
    * django unit tests
    
    * id field comment
    
    * added more tests
    
    * remigrate challenge model
    
    * registered Challenge model to admin interface
    
    * fixed styling
    
    * poetry lock

--- server/bingo/models.py
+++ server/bingo/models.py
@@ -73,3 +73,22 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+
+class Challenge(models.Model):
+    CHALLENGE_TYPES = [
+        ('connect', 'Connect'),
+        ('understand', 'Understand'),
+        ('act', 'Act'),
+    ]
+
+    # django has automatic primary key id field
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    challenge_type = models.CharField(max_length=10, choices=CHALLENGE_TYPES)
+    points = models.PositiveIntegerField()
+    total_completions = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        # Format when printed: Challenge ID: Name (Challenge Type)
+        return f"Challenge {self.id}: {self.name} ({self.challenge_type.capitalize()})"
