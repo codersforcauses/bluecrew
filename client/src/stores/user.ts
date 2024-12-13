bb0688ab1cafa2083d7117ab199fd311dc2ad4b4
mye9 <brannoncran@gmail.com>
2024-12-13 11:35:01 +0000

    Fixed to fit with CI/CD process

--- client/src/stores/user.ts
+++ client/src/stores/user.ts
@@ -1,34 +1,34 @@
-import { defineStore } from 'pinia';
-import { useStorage } from '@vueuse/core';
-import { computed } from 'vue'; 
+import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
+import { computed } from 'vue'
 
 // User interface (include userName, name and email)
 interface User {
-    userName: string;
-    name: string;
-    email: string;
-  }
+  userName: string
+  name: string
+  email: string
+}
 
 export const useUserStore = defineStore('user', () => {
-  const userData = useStorage<User | null>('userData', null);
-  const accessToken = useStorage<string | null>('accessToken', null);
-  const refreshToken = useStorage<string | null>('refreshToken', null);
+  const userData = useStorage<User | null>('userData', null)
+  const accessToken = useStorage<string | null>('accessToken', null)
+  const refreshToken = useStorage<string | null>('refreshToken', null)
 
-  const isLoggedIn = computed(() => !!userData.value);
+  const isLoggedIn = computed(() => !!userData.value)
 
-  // Login Status  
+  // Login Status
   const login = (user: User, tokens: { accessToken: string; refreshToken: string }) => {
-    userData.value = user;
-    accessToken.value = tokens.accessToken;
-    refreshToken.value = tokens.refreshToken;
-  };
+    userData.value = user
+    accessToken.value = tokens.accessToken
+    refreshToken.value = tokens.refreshToken
+  }
 
   //Logout Status
   const logout = () => {
-    userData.value = null;
-    accessToken.value = null;
-    refreshToken.value = null;
-  };
+    userData.value = null
+    accessToken.value = null
+    refreshToken.value = null
+  }
 
   return {
     userData,
@@ -37,8 +37,5 @@ export const useUserStore = defineStore('user', () => {
     refreshToken,
     login,
     logout,
-  };
-
-  
-});
-
+  }
+})
