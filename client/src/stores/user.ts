36cd7b6eff75957dc0dbc29a0f820314feae4d22
Minghui Ye <68409530+mye9@users.noreply.github.com>
2024-12-13 23:23:06 +0800

    Issue 14 create user pinia store (#38)
    * user.ts v1
    
    * fixed file v1
    
    * Comments Add
    
    * Test included
    
    * Fixed to fit with CI/CD process
    
    * Properties Update & Poetry Fix
    
    * Test files Update
    
    * Repair user.ts
    
    * Emergency Update
    
    * Emergency Update v2
    
    * Comments Update
    
    * Remove Test

---
+++ client/src/stores/user.ts
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
+import { computed } from 'vue'
+import type { User } from '@/types/user'
+
+export const useUserStore = defineStore('user', () => {
+  const userData = useStorage<User | null>('userData', null)
+  const accessToken = useStorage<string | null>('accessToken', null)
+  const refreshToken = useStorage<string | null>('refreshToken', null)
+
+  const isLoggedIn = computed(() => userData.value !== null)
+
+  //Logout Status
+  const logout = () => {
+    userData.value = null
+    accessToken.value = null
+    refreshToken.value = null
+  }
+
+  return {
+    userData,
+    isLoggedIn,
+    accessToken,
+    refreshToken,
+    logout,
+  }
+})
