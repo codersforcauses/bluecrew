a1bb5519bcfcbe10afe233b127c2dae8ace7d45c
laurenpudz <lhpudney@gmail.com>
2024-12-14 09:21:06 +0800

    create leaderboard row component

---
+++ client/src/components/LeaderboardRow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+    rank: number
+    avatarIndex: number
+    name: string
+    points: number
+    isHighlighted: boolean
+}>()
+</script>
+
+<template>
+    <v-col :class="[isHighlighted ? 'bg-primaryPink' : 'bg-creamWhite text-primaryGrey']" class="rounded-lg d-flex ga-3 px-5 align-center">
+        <v-text class="font-weight-bold">{{ rank }}</v-text>
+        <v-img
+            class="rounded-circle "
+            max-height="32"
+            max-width="32"
+            cover
+            src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+        ></v-img>
+        <v-text class="me-auto font-weight-bold">{{ name }}</v-text>
+        <v-text :class="[isHighlighted ? 'font-weight-bold' : '' ]" class="text-right">{{ points }} pts</v-text>
+    </v-col>
+</template>
