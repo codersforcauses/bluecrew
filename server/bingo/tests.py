c98fc9ab8225f0fad0cfaad063ad0ec8bfc65dcd
Zhihao Lin <96046435+someonebear@users.noreply.github.com>
2024-12-14 14:13:19 +0800

    Issue 12 - Created api view and serializer for registering users. (#41)
    * Created api view and serializer for registering users.
    
    * Added additional tests and validators.
    Moved bingo urls into bingo/
    
    * Moved validation into serializer.

--- server/bingo/tests.py
+++ server/bingo/tests.py
@@ -1,6 +1,7 @@
+from django.test import TestCase, Client
 from django.db.utils import IntegrityError
-from django.test import TestCase
 from .models import User, Challenge, Friendship
+
 from django.core.exceptions import ValidationError
 
 
@@ -40,6 +41,44 @@ class ChallengeTest(TestCase):
             points=15,
         )
         self.assertEqual(challenge.total_completions, 0)
+
+
+class RegisterUserTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_register_user(self):
+        response = self.client.post(
+            '/api/register/', {'username': 'test_user', 'email': 'test@gmail.com',
+                               'first_name': 'larry', 'last_name': 'bird', 'password': 'SuperSecure123'}
+        )
+
+        self.assertEqual(response.status_code, 201)
+
+    # Required fields not sent.
+    def test_register_user_error(self):
+        response = self.client.post(
+            '/api/register/', {'username': 'forgotten_fields', 'email': 'test@gmail.com',
+                               'password': 'SuperSecure123'}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_email_error(self):
+        response = self.client.post(
+            '/api/register/', {'username': 'forgotten_fields', 'email': 'invalidemail',
+                               'first_name': 'larry', 'last_name': 'bird', 'password': 'SuperSecure123'}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_insecure_password_error(self):
+        response = self.client.post(
+            '/api/register/', {'username': 'forgotten_fields', 'email': 'test@test.com',
+                               'first_name': 'larry', 'last_name': 'bird', 'password': '1234'}
+        )
+
+        self.assertEqual(response.status_code, 400)
 
 
 class FriendshipTest(TestCase):
--- server/bingo/views.py
+++ server/bingo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render # noqa
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserRegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'User created successfully.'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
