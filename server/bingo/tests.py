b2aee44383e338cefcd715befb8192748b4571e2
James Lee <jameslee6352@gmail.com>
2024-12-14 14:29:45 +0800

    More random tests

--- server/bingo/tests.py
+++ server/bingo/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Challenge, User
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from datetime import date
 
 
@@ -47,23 +48,42 @@ class UsersTest(TestCase):
         self.user = User.objects.create(
             username="Test",
             password="A generic, valid password",
+            email="test@example.com",
             first_name="Jane",
             last_name="Doe",
             birthdate=date(1, 1, 1)
         )
 
-    def creation(self):
+    def test_creation(self):
         try:
             User.objects.get(username="Test")
         except User.DoesNotExist:
             self.fail("User was not properly created")
 
-    def random_queries_fail(self):
+    def test_random_queries_fail(self):
         self.assertRaises(User.DoesNotExist, User.objects.get, username="Barry")
 
-    def base_users_not_staff(self):
+    def test_base_users_not_staff(self):
         self.assertFalse(self.user.is_staff)
 
-    def superusers_are_staff(self):
+    def test_superusers_are_staff(self):
         self.user.is_superuser = True
         self.assertTrue(self.user.is_staff)
+
+    def test_enums_within_bounds(self):
+        self.user.gender_identity = 7
+        self.assertRaises(ValidationError, self.user.full_clean)
+
+    def test_change_gender_identity(self):
+        self.user.gender_identity = User.Gender.FEMALE
+        self.user.full_clean()
+        self.user.save()
+
+    def test_username_uniqueness(self):
+        self.assertRaises(
+            IntegrityError, 
+            User.objects.create, 
+            username="Test", 
+            password="A different valid password", 
+            email="test@example.net"
+        )
