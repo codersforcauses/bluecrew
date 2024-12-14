7c8e79e60e83c9fb572eaa9f9b6136cbeb8cfc64
23348918 <23348918@student.uwa.edu.au>
2024-12-14 06:40:40 +0000

    added delete friendship view

--- server/bingo/views.py
+++ server/bingo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render  # noqa
+from .models import Friendship
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_friendship(request, friendship_id):
+    try:
+        friendship = Friendship.objects.get(id=friendship_id)
+
+        if request.user != friendship.requester and request.user != friendship.receiver:
+            return Response({"error": "You do not have permission to delete this friendship."}, status=403)
+
+        friendship.delete()
+        return Response({"message": "Friendship deleted successfully."}, status=204)
+
+    except Friendship.DoesNotExist:
+        return Response({"error": "Friendship not found."}, status=404)
