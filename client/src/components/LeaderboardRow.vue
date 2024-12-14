e4895d2177ec1764386aa2d13ba56035895f4eee
Lauren <lhpudney@gmail.com>
2024-12-14 14:00:27 +0800

    Add elipsis for long names

--- client/src/components/LeaderboardRow.vue
+++ client/src/components/LeaderboardRow.vue
@@ -15,10 +15,23 @@ defineProps<{
             class="rounded-circle "
             max-height="32"
             max-width="32"
+            min-width="32"
             cover
             src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
         ></v-img>
-        <v-text class="me-auto font-weight-bold">{{ name }}</v-text>
-        <v-text :class="[isHighlighted ? 'font-weight-bold' : '' ]" class="text-right">{{ points }} pts</v-text>
+        <v-text class="me-auto font-weight-bold truncate-single-line">{{ name }}</v-text>
+        <v-text :class="[isHighlighted ? 'font-weight-bold' : '' ]" class="text-right points">{{ points }} pts</v-text>
     </v-col>
 </template>
+
+<style scoped>
+.truncate-single-line {
+    white-space: nowrap;        
+    overflow: hidden;          
+    text-overflow: ellipsis;     
+}
+
+.points {
+    white-space: nowrap;
+}
+</style>
