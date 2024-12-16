f12d64f00a69afacf1b601803749a6059815eeb4
aradavi63 <adam.ahmadavi@gmail.com>
2024-12-16 13:23:03 +0000

    create vue file for landing page

---
+++ client/src/components/LandingPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+//import { useDisplay } from 'vuetify' // Used to figure out if page is landscape/portrait
+
+</script>
+
+<template>
+    <v-card><img src="/bc-logo.png" alt="logo" style="margin: 0 auto" /></v-card>
+    <p>hello</p>
+</template>
+
+<style scoped></style>
