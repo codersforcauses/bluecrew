3b96c3dabf08629c27fdda19386c7410304ee4be
James Lee <82937700+SafetyInObscurity@users.noreply.github.com>
2025-01-04 12:46:42 +0800

    Issue 36 add missing fields to user model (#46)
    * Implement Gender field
    
    * Add indigenous identity
    
    * Adding tests
    
    * Ensuring is_superuser grants admin privileges
    
    * More random tests
    
    * Correcting whitespace
    
    * Visibility and email uniqueness
    
    * Flake8 compliance
    
    * Fixing migrations
    
    * Fixing indigenous identity
    
    * Fixing indigenous identity default

--- server/bingo/tests.py
+++ server/bingo/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase, Client
-from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from datetime import date
 from django.utils import timezone
 from .models import User, Challenge, Friendship, ChallengeInteraction
-
-from django.core.exceptions import ValidationError
 from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
@@ -47,6 +47,65 @@ class ChallengeTest(TestCase):
             points=15,
         )
         self.assertEqual(challenge.total_completions, 0)
+
+
+class UsersTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="Test",
+            password="A generic, valid password",
+            email="test@example.com",
+            first_name="Jane",
+            last_name="Doe",
+            birthdate=date(1, 1, 1)
+        )
+
+    def test_creation(self):
+        try:
+            User.objects.get(username="Test")
+        except User.DoesNotExist:
+            self.fail("User was not properly created")
+
+    def test_random_queries_fail(self):
+        self.assertRaises(User.DoesNotExist, User.objects.get, username="Barry")
+
+    def test_base_users_not_staff(self):
+        self.assertFalse(self.user.is_staff)
+
+    def test_superusers_are_staff(self):
+        self.user.is_superuser = True
+        self.assertTrue(self.user.is_staff)
+
+    def test_gender_bounds(self):
+        self.user.gender_identity = 7
+        self.assertRaises(ValidationError, self.user.full_clean)
+
+    def test_visibility_bounds(self):
+        self.user.visibility = -1
+        self.assertRaises(ValidationError, self.user.full_clean)
+
+    def test_change_gender_identity(self):
+        self.user.gender_identity = User.Gender.FEMALE
+        self.user.full_clean()
+        self.user.save()
+
+    def test_username_uniqueness(self):
+        self.assertRaises(
+            IntegrityError,
+            User.objects.create,
+            username="Test",
+            password="A different valid password",
+            email="test@example.net"
+        )
+
+    def test_email_uniqueness(self):
+        self.assertRaises(
+            IntegrityError,
+            User.objects.create,
+            username="Assess",
+            password="A string of random characters",
+            email="test@example.com"
+        )
 
 
 class RegisterUserTest(TestCase):
