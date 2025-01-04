d4bbd03d744cf0ea9bd753c96af6c0a2a43bead2
Adam <153423192+aradavi63@users.noreply.github.com>
2025-01-04 15:34:11 +0800

    Issue 48 make landing page (#53)
    * create vue file for landing page
    
    * add landing view page
    
    * deleted "landing page" located in src/components, the wrong place
    
    * adding the image header and placeholder button
    
    * fix banner
    
    * fix layout and buttons
    
    * upadting mobile page
    
    * change mobile to xs
    
    * fix button sizing
    
    * run format to send pull request
    
    * change router view and RegisterModal location
    
    * move RegisterModal component to App.vue and remove extraneous features
    
    * switch to using v-container grids
    
    * sizing of page
    
    * add oceanyouth logo
    
    * delete home view
    
    * remove redundancy in  app.vue
    
    * update tagline in mobile view
    
    * done i think
    
    * fix page width

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { useModalStore } from '@/stores/modal'
 import { RouterView } from 'vue-router'
 import BluecrewFooter from '@/components/BluecrewFooter.vue'
+import RegisterModal from '@/components/RegisterModal.vue'
+
+const modalStore = useModalStore()
 </script>
 
 <template>
   <RouterView />
   <BluecrewFooter />
+  <RegisterModal v-if="modalStore.currentModal === 'register'" @close="modalStore.closeModal" />
 </template>
 
 <style scoped></style>
---
+++ client/src/views/LandingView.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { useModalStore } from '@/stores/modal'
+import { useDisplay } from 'vuetify'
+
+const { xs } = useDisplay()
+const modalStore = useModalStore()
+
+function openRegisterButton() {
+  modalStore.openRegister()
+}
+</script>
+
+<template>
+  <div v-if="!xs" class="header">
+    <img src="/beach-header.jpg" alt="Ocean Beach" class="header-image" />
+  </div>
+  <v-container>
+    <v-row height="100%">
+      <v-col :cols="xs ? 12 : 6" class="d-flex justify-center align-center">
+        <img
+          src="/bc-logo.png"
+          alt="Blue Crew Logo"
+          :class="{
+            'logo-mobile': xs,
+            'logo-desktop': !xs,
+          }"
+        />
+        <img
+          src="/oy-logo.png"
+          alt="Ocean Youth Logo"
+          :class="{
+            'logo-mobile': xs,
+            'logo-desktop': !xs,
+          }"
+        />
+      </v-col>
+
+      <v-col :cols="xs ? 12 : 6" class="d-flex flex-column justify-center align-center">
+        <div class="text-center">
+          <h2 class="tagline">
+            The inspiring way
+            <template v-if="!xs">
+              <br />to become an ocean champion and <br />protect our blue planet
+            </template>
+            <template v-else>
+              <br />to become an ocean <br />champion and protect our <br />blue planet
+            </template>
+          </h2>
+          <div class="buttons">
+            <v-btn
+              id="register-button"
+              class="bg-primaryPink text-creamyWhite"
+              rounded="xl"
+              size="x-large"
+              @click="openRegisterButton"
+            >
+              Get Started
+            </v-btn>
+            <v-btn
+              id="login-button"
+              class="bg-primaryBlue text-creamyWhite"
+              rounded="xl"
+              size="x-large"
+              @click="openRegisterButton"
+            >
+              I already have an account
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 200px;
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 50px;
+}
+
+.header-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: boxed;
+}
+
+.logo-mobile {
+  width: 150px;
+  margin: 40px 20px;
+}
+
+.logo-desktop {
+  width: 250px;
+  margin: 0 20px;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  max-width: 400px;
+  margin: 50px auto;
+}
+</style>
