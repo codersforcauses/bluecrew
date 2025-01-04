3a13a41644eca0ccee8e08001e730f02acdd49e8
aradavi63 <adam.ahmadavi@gmail.com>
2025-01-04 06:52:03 +0000

    delete home view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LandingView from '@/views/LandingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-    },
     {
       path: '/',
       name: 'landing',
