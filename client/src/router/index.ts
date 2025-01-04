f4c2c02266624b8349aeab2bff4424a8a0d6f341
zichen <zzcnhy@gmail.com>
2025-01-04 13:35:43 +0800

    empty_PAGE

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FriendView from '../views/FriendView.vue'
+import PreferencesView from '../views/PreferencesView.vue'
+import { useModalStore } from '@/stores/modal'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +12,30 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/friends',
+      name: 'friends',
+      component: FriendView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/preferences',
+      name: 'preferences',
+      component: PreferencesView,
+      meta: { requiresAuth: true },
+    },
   ],
+})
+
+router.beforeEach((to, from) => {
+  const modalStore = useModalStore()
+
+  if (modalStore.currentModal === 'none') {
+    modalStore.openRegister()
+    return false
+  }
+
+  return true
 })
 
 export default router
