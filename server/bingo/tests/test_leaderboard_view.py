0e17c500dbb7fafb4f986c9c51c52cf94f7c91ce
bibonut <laurelchow04@gmail.com>
2025-01-06 14:57:41 +0000

    replaced django test client with DRF test client, refactored code

--- server/bingo/tests/test_leaderboard_view.py
+++ server/bingo/tests/test_leaderboard_view.py
@@ -1,6 +1,6 @@
-from django.test import TestCase, Client
+from django.test import TestCase
+from rest_framework.test import APIClient
 from ..models import User
-import json
 
 
 class LeaderboardTest(TestCase):
@@ -12,42 +12,33 @@ class LeaderboardTest(TestCase):
         self.user3 = User.objects.create_user(
             username="user3", email="user3@example.com", password="password", total_points=3)
 
-        self.client = Client()
+        self.client = APIClient()
 
     def test_leaderboard(self):
         # GET leaderboard api
-        leaderboard_response = self.client.get("/api/leaderboard", follow=True)
-        leaderboard_response_content = json.loads(leaderboard_response.content.decode("utf8"))
+        leaderboard_response = self.client.get("/api/leaderboard/")
+        leaderboard_response_content = leaderboard_response.data
 
-        self.assertEqual(leaderboard_response_content[0]["username"], "user3")
-        self.assertEqual(leaderboard_response_content[0]["total_points"], 3)
+        self.assertEqual(leaderboard_response_content[0]["username"], self.user3.username)
+        self.assertEqual(leaderboard_response_content[0]["total_points"], self.user3.total_points)
         self.assertEqual(leaderboard_response_content[0]["rank"], 1)
 
-        self.assertEqual(leaderboard_response_content[1]["username"], "user1")
-        self.assertEqual(leaderboard_response_content[1]["total_points"], 1)
+        self.assertEqual(leaderboard_response_content[1]["username"], self.user1.username)
+        self.assertEqual(leaderboard_response_content[1]["total_points"], self.user1.total_points)
         self.assertEqual(leaderboard_response_content[1]["rank"], 2)
 
-        self.assertEqual(leaderboard_response_content[2]["username"], "user2")
-        self.assertEqual(leaderboard_response_content[2]["total_points"], 1)
+        self.assertEqual(leaderboard_response_content[2]["username"], self.user2.username)
+        self.assertEqual(leaderboard_response_content[2]["total_points"], self.user2.total_points)
         self.assertEqual(leaderboard_response_content[2]["rank"], 2)
 
     def test_logged_in_leaderboard(self):
-        self.client.login(username="user1", password="password")
-
-        # get auth token
-        token_response = self.client.post(
-            "/api/token/", {"username": "user1", "password": "password"}, content_type="application/json")
-        token_response_content = json.loads(
-            token_response.content.decode('utf-8'))
+        self.client.force_authenticate(user=self.user1)
 
         # GET leaderboard api
-        user_token = token_response_content["access"]
-        leaderboard_response = self.client.get(
-            "/api/leaderboard", headers={"Authorization": "Bearer " + user_token}, follow=True)
-        leaderboard_response_content = json.loads(
-            leaderboard_response.content.decode("utf8"))
+        leaderboard_response = self.client.get("/api/leaderboard/")
+        leaderboard_response_content = leaderboard_response.data
 
         # last in leaderboard should be current logged in user
-        self.assertEqual(leaderboard_response_content[-1]["username"], "user1")
-        self.assertEqual(leaderboard_response_content[-1]["total_points"], 1)
+        self.assertEqual(leaderboard_response_content[-1]["username"], self.user1.username)
+        self.assertEqual(leaderboard_response_content[-1]["total_points"], self.user1.total_points)
         self.assertEqual(leaderboard_response_content[-1]["rank"], 2)
