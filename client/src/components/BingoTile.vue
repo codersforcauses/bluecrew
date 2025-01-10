290a81bab241c04d13e4c0820a6e2fe85e1958a4
laurenpudz <lhpudney@gmail.com>
2025-01-10 22:25:18 +0800

    svg switching and stying fixes for sat

--- client/src/components/BingoTile.vue
+++ client/src/components/BingoTile.vue
@@ -1,29 +1,50 @@
 <script setup lang="ts">
+
 defineProps<{
-  type: string
-  text: string
-  status: string
-  selected: boolean
+    type: string
+    text: string
+    status: string
+    selected: boolean
 }>()
+
+// need to download svgs and change out srcs and names
+function getIconSrc(type: string): string {
+    switch (type) {
+        case "thing1":
+            return "man.svg"
+        case "thing2":
+            return "thing2.svg"
+        default:
+            return "man.svg"
+    }
+}
+
+
 </script>
 
+<!-- Added flex and text styling, call function for name of icon src, add image width -->
 <template>
-    <div class="bg-creamWhite bingo rounded-lg">
-        <div>
-            <v-img src="man.svg">
-            </v-img>
-            <p class="text-center">
-                {{ text }} 
-            </p>
-        </div>
+    <div class="bg-creamWhite outer-tile rounded-lg d-flex flex-column align-center ">
+
+        <v-img :src="getIconSrc(type)" :style="{ width: '30%' }" />
+    <p class=" tile-text text-center font-weight-bold text-primaryGrey">
+            {{ text }}
+        </p>
+
     </div>
 </template>
 
+<!-- changed sizes to rem to be proportional and added padding, improved naming??, random finicky fixes -->
 <style scoped>
-.bingo {
-    height: 100px;
-    width: 100px;
-    border: black 1px solid;
+.outer-tile {
+    height: 5rem;
+    width: 5rem;
+    border: #4F4F4F 0.1rem solid;
+    padding: 0.5rem;
+}
+
+.tile-text {
+    font-size: 0.6rem;
+    line-height: 1.2
 }
 </style>
-
