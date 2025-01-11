3efc85c0469bde0f2cbbb2f85747dbb0a106df6a
aradavi63 <adam.ahmadavi@gmail.com>
2025-01-11 04:23:31 +0000

    adding in slots

--- client/src/views/PlaceHolderview.vue
+++ client/src/views/PlaceHolderview.vue
@@ -2,48 +2,55 @@
 import FriendComponent from '@/components/FriendComponent.vue'
 import { ref } from 'vue'
 
-// Define TypeScript interface for leaderboard data
-interface LeaderboardEntry {
+// Define TypeScript interface for friend data
+interface FriendEntry {
   avatarIndex: number
   name: string
-  isHighlighted: boolean
 }
 
-const leaderboardData = ref<LeaderboardEntry[]>([
-  { avatarIndex: 2, name: 'Marsha Fisher', isHighlighted: false },
-  { avatarIndex: 3, name: 'Juanita Cormier', isHighlighted: false },
-  { avatarIndex: 1, name: 'Tamara Schmidt', isHighlighted: false },
-  { avatarIndex: 4, name: 'Ricardo Veum', isHighlighted: false },
+const friendData = ref<FriendEntry[]>([
+  { avatarIndex: 1, name: 'Marsha Fisher'},
+  { avatarIndex: 2, name: 'Juanita Cormier'},
 ])
 </script>
 
 <template>
   <v-container>
-    <h2 class="leaderboard-text text-primaryPink mb-4 mb-sm-3 mb-md-4">Friends</h2>
+    <h2 class="friend-text text-primaryPink mb-4 mb-sm-3 mb-md-4">Friends</h2>
 
-    <!-- Other Users -->
-    <h3 class="section-title2 text-primaryBlue">Overall Rankings</h3>
-    <v-row class="leaderboard-scroll">
-      <v-col v-for="(row, index) in leaderboardData" :key="index" cols="12">
+    <h3 class="section-title2 text-primaryBlue">Friends List</h3>
+    <v-row class="friend-scroll">
+      <!-- Default Variant -->
+      <v-col v-for="(row, index) in friendData" :key="index" cols="12">
         <FriendComponent
           :avatar-index="row.avatarIndex"
           :name="row.name"
-          :is-highlighted="row.isHighlighted"
         />
       </v-col>
+      <!-- Accept/Reject Variant -->
+      <v-col v-for="(row, index) in friendData" :key="index" cols="12">
+        <FriendComponent
+          :avatar-index="row.avatarIndex"
+          :name="row.name"
+          
+        />
+      </v-col>
+      <!-- Dismiss Variant -->
+
+      <!-- Delete Variant -->
     </v-row>
   </v-container>
 </template>
 
 <style scoped>
-.leaderboard-text {
+.friend-text {
   text-align: center;
   font-family: 'Lilita One', cursive;
   font-size: 40px;
   font-weight: 500;
 }
 
-.leaderboard-scroll {
+.friend-scroll {
   max-height: 400px;
   overflow-y: auto;
 }
