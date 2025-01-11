2ae4c76b2cdd476524d27b103fab740aee0686ad
aradavi63 <adam.ahmadavi@gmail.com>
2025-01-11 03:51:54 +0000

    Adding view to test friends

---
+++ client/src/components/FriendComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  avatarIndex: number
+  name: string
+  isHighlighted: boolean
+}>()
+</script>
+
+<template>
+  <v-col
+    :class="[isHighlighted ? 'bg-primaryPink' : 'bg-creamWhite text-primaryGrey']"
+    class="rounded-lg d-flex ga-3 px-5 align-center"
+  >
+    <v-img
+      class="rounded-circle"
+      max-height="32"
+      max-width="32"
+      min-width="32"
+      cover
+      src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+    ></v-img>
+    <v-text class="me-auto font-weight-bold truncate-name">{{ name }}</v-text>
+  </v-col>
+</template>
+
+<style scoped>
+.truncate-name {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.points {
+  white-space: nowrap;
+}
+</style>
--- client/src/views/PlaceHolderview.vue
+++ client/src/views/PlaceHolderview.vue
@@ -1,5 +1,68 @@
+<script setup lang="ts">
+import FriendComponent from '@/components/FriendComponent.vue'
+import { ref } from 'vue'
+
+// Define TypeScript interface for leaderboard data
+interface LeaderboardEntry {
+  avatarIndex: number
+  name: string
+  isHighlighted: boolean
+}
+
+const leaderboardData = ref<LeaderboardEntry[]>([
+  { avatarIndex: 2, name: 'Marsha Fisher', isHighlighted: false },
+  { avatarIndex: 3, name: 'Juanita Cormier', isHighlighted: false },
+  { avatarIndex: 1, name: 'Tamara Schmidt', isHighlighted: false },
+  { avatarIndex: 4, name: 'Ricardo Veum', isHighlighted: false },
+])
+</script>
+
 <template>
-  <div>
-    <p>Placeholder for {{ $route.name }}</p>
-  </div>
+  <v-container>
+    <h2 class="leaderboard-text text-primaryPink mb-4 mb-sm-3 mb-md-4">Friends</h2>
+
+    <!-- Other Users -->
+    <h3 class="section-title2 text-primaryBlue">Overall Rankings</h3>
+    <v-row class="leaderboard-scroll">
+      <v-col v-for="(row, index) in leaderboardData" :key="index" cols="12">
+        <FriendComponent
+          :avatar-index="row.avatarIndex"
+          :name="row.name"
+          :is-highlighted="row.isHighlighted"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<style scoped>
+.leaderboard-text {
+  text-align: center;
+  font-family: 'Lilita One', cursive;
+  font-size: 40px;
+  font-weight: 500;
+}
+
+.leaderboard-scroll {
+  max-height: 400px;
+  overflow-y: auto;
+}
+
+.section-title {
+  margin-top: 16px;
+  margin-bottom: 2px;
+  font-size: 20px;
+  font-family: 'Poppins', cursive;
+  text-align: left;
+  font-weight: bold;
+}
+
+.section-title2 {
+  margin-top: 32px;
+  margin-bottom: 2px;
+  font-size: 20px;
+  font-family: 'Poppins', cursive;
+  text-align: left;
+  font-weight: bold;
+}
+</style>
