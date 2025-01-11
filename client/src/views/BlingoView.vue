5c922e6fec0a1a85f49cd978eafb0a41ea9fc172
laurenpudz <lhpudney@gmail.com>
2025-01-11 13:25:42 +0800

    add blingo view

---
+++ client/src/views/BlingoView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BlingoTile from '@/components/BingoTile.vue'
+</script>
+
+<template>
+    <div>
+        <BlingoTile type="" text="placeholder text" status="" selected="true" />
+    </div>
+</template>
+
+<style scoped></style>
