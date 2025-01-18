d8f74981432ac3eb68b5750bc94b60f64d666553
aradavi63 <adam.ahmadavi@gmail.com>
2025-01-18 06:50:50 +0000

    add button and other stuff to edit

--- client/src/views/PerferenceView.vue
+++ client/src/views/PerferenceView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import WaveBanner from '@/components/WaveBanner.vue'
 import { useDisplay } from 'vuetify'
+import { ref } from 'vue'
 
 const { xs } = useDisplay()
 </script>
@@ -13,6 +14,34 @@ const { xs } = useDisplay()
         </v-row>
         
         <v-row class="flex-grow-1 w-100" align="center">
+            <v-col align-start>
+                <v-img
+                class="rounded-circle"
+                max-height="96"
+                max-width="96"
+                min-width="96"
+                cover
+                src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                ></v-img>
+            </v-col>
+
+            <v-col>
+                <v-text class="me-auto font-weight-bold truncate-name">{{ username }}</v-text>
+                <v-text class="me-auto font-weight-bold truncate-name">{{ name }}</v-text>
+            </v-col>
+
+            <v-spacer></v-spacer>
+
+            <v-text></v-text>
+            <v-text></v-text>
+
+            <v-btn class="bg-primaryBlue" prepend-icon="mdi-edit">
+                <template v-slot:prepend>
+                    <v-icon color="rgb(var(--v-theme-primaryGrey))"></v-icon>
+                </template>
+                Edit Profile
+            </v-btn>
+
         </v-row>
     </v-container>
 </template>
