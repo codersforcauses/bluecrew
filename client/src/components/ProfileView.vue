9911dc32873a424341fdf5fa9817bbee59d9acd2
yuwengchoong <yuweng.choong@gmail.com>
2025-01-25 06:01:36 +0000

    updated profile view

--- client/src/components/ProfileView.vue
+++ client/src/components/ProfileView.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  import ChallengesCard from "@/components/ChallengesCard.vue";
+  
+  const title = ref("Challenge 1");
+  const description = ref("This is a challenge");
+  const type = ref<'connect' | 'understand' | 'act'>('connect');
+  const points = ref(200);
+  const startDate = ref("27/11/2024 9:27pm");
+  const finishDate = ref("27/11/2024 10:27pm");
+  const status = ref<'complete' | 'uncomplete'>('complete');
+</script>
+
 <template>
     <v-container>
       <ChallengesCard
@@ -11,36 +24,4 @@
       />
     </v-container>
   </template>oh
-  
-  <script lang="ts">
-  import { defineComponent, ref } from "vue";
-  import ChallengesCard from "@/components/ChallengesCard.vue";
-  
-  export default defineComponent({
-    name: "App",
-    components: {
-        ChallengesCard,
-    },
-    setup() {
-      // Reactive state variables using Composition API
-      const title = ref("Challenge 1");
-      const description = ref("This is a challenge");
-      const type = ref<'connect' | 'understand' | 'act'>('connect');
-      const points = ref(200);
-      const startDate = ref("27/11/2024 9:27pm")
-      const finishDate = ref("27/11/2024 10:27pm");
-      const status = ref<'complete' | 'uncomplete'>('complete');
-  
-      return {
-        title,
-        description,
-        type,
-        points,
-        startDate,
-        finishDate,
-        status,
-      };
-    },
-  });
-  </script>
   
