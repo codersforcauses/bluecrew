ab7e9d9cf472c92c609050e8713faface8e864f2
HuxleyBerry <huxley.berry@gmail.com>
2025-01-25 07:15:34 +0000

    oops

---
+++ client/src/views/BlingoView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import BlingoTile from '@/components/BingoTile.vue'
+</script>
+
+<template>
+    <div class="d-flex flex-column align-center mt-5 mx-5">
+        <div class="d-flex ga-1 w-100">
+            <BlingoTile type="connect" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="act" text="placeholder text" status="started" :selected="false" />
+            <BlingoTile type="understand" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="connect" text="placeholder text" status="completed" :selected="false" />
+        </div>
+        <div class="d-flex ga-1 mt-1 w-100">
+            <BlingoTile type="connect" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="understand" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="act" text="placeholder text" status="completed" :selected="false" />
+            <BlingoTile type="act" text="placeholder text" status="not started" :selected="false" />
+        </div>
+        <div class="d-flex ga-1 mt-1 w-100">
+            <BlingoTile type="act" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="connect" text="placeholder text" status="not started" :selected="true" />
+            <BlingoTile type="understand" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="connect" text="placeholder text" status="not started" :selected="false" />
+        </div>
+        <div class="d-flex ga-1 mt-1 w-100">
+            <BlingoTile type="understand" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="connect" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="act" text="placeholder text" status="not started" :selected="false" />
+            <BlingoTile type="understand" text="placeholder text" status="not started" :selected="false" />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
