49ef610e4692089071e6d6a6c5978daca855a5ce
yuwengchoong <yuweng.choong@gmail.com>
2025-01-25 05:40:17 +0000

    updated challenge card

--- client/src/components/ChallengesCard.vue
+++ client/src/components/ChallengesCard.vue
@@ -1,20 +1,33 @@
+<script setup lang="ts">
+
+  defineProps<{
+    title: string
+    description: string
+    type: 'connect' | 'understand' | 'act'
+    points: number
+    startDate: string
+    finishDate: string
+    status: boolean
+  }>()
+</script>
+
 <template>
   <v-card class="pa-4" outlined>
     <v-card-title class="d-flex justify-space-between align-center">
       <div>
-        <span class="font-weight-bold text-h6">{{ propTitle }}</span>
-        <span class="subtitle ml-2 text-subtitle-2">{{ propSubtitle }}</span>
+        <span class="font-weight-bold text-h6">{{ title }}</span>
+        <span class="subtitle ml-2 text-subtitle-2">{{ description }}</span>
       </div>
-      <span class="text-body-1 font-weight-bold">{{ propPoints }}pts</span>
+      <span class="text-body-1 font-weight-bold">{{ points }}pts</span>
     </v-card-title>
 
     <v-card-text class="d-flex justify-space-between align-center">
       <div class="details">
         <p class="mb-2">
-          <strong>Complete Date:</strong> {{ propCompleteDate }}
+          <strong>Complete Date:</strong> {{ finishDate }}
         </p>
         <p>
-          <strong>Time Spent on Challenge:</strong> {{ propTimeSpent }}
+          <strong>Start Date:</strong> {{ startDate }}
         </p>
       </div>
       <v-chip
@@ -23,65 +36,11 @@
         class="status-indicator"
         outlined
       >
-        ✔ {{ propStatus }}
+        ✔ {{ status }}
       </v-chip>
     </v-card-text>
   </v-card>
 </template>
-
-<script lang="ts">
-import { defineComponent, toRefs } from "vue";
-
-export default defineComponent({
-  name: "ChallengeCard",
-  props: {
-    title: {
-      type: String,
-      default: "Challenge 1",
-    },
-    subtitle: {
-      type: String,
-      default: "Bingo 1",
-    },
-    points: {
-      type: Number,
-      default: 200,
-    },
-    completeDate: {
-      type: String,
-      default: "27/11/2024 10:27pm",
-    },
-    timeSpent: {
-      type: String,
-      default: "60min",
-    },
-    status: {
-      type: String,
-      default: "Complete",
-    },
-  },
-  setup(props) {
-    // Destructure props with new names to avoid name collisions
-    const {
-      title: propTitle,
-      subtitle: propSubtitle,
-      points: propPoints,
-      completeDate: propCompleteDate,
-      timeSpent: propTimeSpent,
-      status: propStatus,
-    } = toRefs(props);
-
-    return {
-      propTitle,
-      propSubtitle,
-      propPoints,
-      propCompleteDate,
-      propTimeSpent,
-      propStatus,
-    };
-  },
-});
-</script>
 
 <style scoped>
 .subtitle {
