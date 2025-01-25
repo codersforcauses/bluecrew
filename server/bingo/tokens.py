031047d9eb21e8ca406bee6659217624a2818fdd
SafetyInObscurity <jameslee6352@gmail.com>
2025-01-25 05:22:16 +0000

    Make token generator

---
+++ server/bingo/tokens.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+
+class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        email_field = user.get_email_field_name()
+        email = getattr(user, email_field)
+        return f"{user.pk}{timestamp}{email}"
