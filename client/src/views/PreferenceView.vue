598f7f481378e92df1a4746cae17431789347eff
aradavi63 <adam.ahmadavi@gmail.com>
2025-01-27 14:07:20 +0000

    fix avatar spacing and options

--- client/src/views/PreferenceView.vue
+++ client/src/views/PreferenceView.vue
@@ -11,10 +11,11 @@ const fullName = ref('Firstname Lastname')
 const bio = ref('This is a bio.')
 const totalPoints = ref('1000pts')
 const isEditing = ref(false)
-const selectedAvatar = ref(0)
 const visibility = ref('Bluecrew only')
 const visOptions = ['Bluecrew only', 'Friend only', 'Public']
 
+const selectedAvatar = ref(0)
+const avatar = ref('/clown-fish.svg')
 const avatarOptions = [
   {
     src: 'clown-fish.svg',
@@ -48,6 +49,7 @@ const handleCancel = () => {
 }
 
 const handleApply = () => {
+  avatar.value = avatarOptions[selectedAvatar.value].src
   isEditing.value = false
 }
 </script>
@@ -73,7 +75,7 @@ const handleApply = () => {
               max-width="96"
               min-width="96"
               cover
-              src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+              :src="avatar"
             ></v-img>
           </div>
         </v-col>
@@ -115,7 +117,7 @@ const handleApply = () => {
             <v-row>
               <v-col cols="12">
                 <p class="text-h6 font-weight-bold mb-4">Avatar</p>
-                <div class="d-flex flex-wrap gap-4">
+                <div class="d-flex flex-wrap gap-4 justify-center">
                   <v-img
                     v-for="(avatar, index) in avatarOptions"
                     :key="index"
@@ -148,7 +150,7 @@ const handleApply = () => {
             <!-- Visibility Section -->
             <v-row>
               <v-col cols="12">
-                <p class="text-h6 font-weight-bold mb-4">Visibility</p>
+                <p class="text-h6 font-weight-bold mb-4 justify-center">Visibility</p>
                 <v-item-group
                   mandatory
                   class="d-flex flex-column"
