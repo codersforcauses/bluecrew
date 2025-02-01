cc757dc7ff9e4411a90c5ae7f715e83542e75286
aradavi63 <adam.ahmadavi@gmail.com>
2025-02-01 05:07:50 +0000

    fix message store for composition api

--- client/src/stores/message.ts
+++ client/src/stores/message.ts
@@ -1,25 +1,33 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useMessageStore = defineStore ('message', {
-    state: (): Message => ({
-        isOpen: false,
-        content: '',
-    }),
+export type MessageType = 'success' | 'warning' | 'error'
 
-    actions: {
-        showMessage(message: string) {
-            this.content = message
-            this.isOpen = true
-        },
+export const useMessageStore = defineStore('message', () => {
+  const isOpen = ref(false)
+  const content = ref('')
+  const title = ref('')
+  const type = ref<MessageType>('success')
 
-        closeMessage() {
-            this.isOpen = false
-            this.content = ''
-        }
-    }
-})
-
-interface Message {
-    isOpen: boolean
-    content: string
+  function showMessage(message: string, messageTitle: string, messageType: MessageType = 'success') {
+    isOpen.value = true
+    content.value = message
+    title.value = messageTitle 
+    type.value = messageType
   }
+
+  function closeMessage() {
+    isOpen.value = false
+    content.value = ''
+    title.value = ''
+  }
+
+  return {
+    isOpen,
+    content,
+    title,
+    type,
+    showMessage,
+    closeMessage
+  }
+})
