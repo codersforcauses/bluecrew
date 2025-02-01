e7b9ee8f98d4925eac9b7482d909381e9414ee53
Minghui Ye <68409530+mye9@users.noreply.github.com>
2025-02-01 14:48:26 +0800

    Issue 129 Add Email Vertification  (#150)
    * Components All Done
    
    * Format Check
    
    * minor fix
    
    ---------
    
    Co-authored-by: HuxleyBerry <huxley.berry@gmail.com>

--- client/src/stores/user.ts
+++ client/src/stores/user.ts
@@ -62,9 +62,7 @@ export const useUserStore = defineStore('user', () => {
     try {
       // API call to register using axios server instance
       const response = await server.post('/register/', body)
-
       console.log('Registration successful:', response.data)
-      alert('Registration successful!')
       return true
     } catch (error) {
       if (isAxiosError(error)) {
