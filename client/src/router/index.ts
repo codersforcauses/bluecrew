84a5b272bd9dd3911bfcc3f9632a092903a18332
samueltl21 <samueltheodore21@gmail.com>
2025-02-14 22:35:51 +0000

    fixing route for forgotpswd

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -8,7 +8,8 @@ import FriendView from '@/views/FriendView.vue'
 import BlingoView from '@/views/BlingoView.vue'
 import PreferenceView from '@/views/PreferenceView.vue'
 import ProfileView from '@/views/ProfileView.vue'
-import ForgotPasswordView from '@/views/ProfileView.vue'
+import ForgotPasswordView from '@/views/ForgotPasswordView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
