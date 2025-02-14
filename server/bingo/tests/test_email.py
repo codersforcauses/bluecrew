34255edf3d8794cfedbef8f7e076ffb69972f61c
SafetyInObscurity <jameslee6352@gmail.com>
2025-02-14 11:46:09 +0000

    Testing password resetting

--- server/bingo/tests/test_email.py
+++ server/bingo/tests/test_email.py
@@ -4,6 +4,7 @@ from django.core import mail
 from ..models import User
 from datetime import date
 from html.parser import HTMLParser
+from urllib.parse import urlparse, parse_qs
 from django.conf import settings
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
@@ -40,7 +41,7 @@ class TestEmailVerification(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].from_email, settings.VERIFICATION_EMAIL)
+        self.assertEqual(mail.outbox[0].from_email, settings.ACCOUNTS_EMAIL)
 
         id_parser = FindHREFByID("verify_link")
         id_parser.feed(mail.outbox[0].body)
@@ -97,3 +98,44 @@ class TestEmailVerification(TestCase):
             {"uid64": urlsafe_base64_encode(force_bytes(self.user.pk)), "token": "42"}
         )
         self.assertEqual(response.status_code, 404)
+
+
+class TestPasswordReset(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(
+            username="Test",
+            password="A generic valid password",
+            first_name="Jane",
+            last_name="Doe",
+            email="janedoe@example.com",
+            birthdate=date(1, 1, 1),
+            is_active=True,
+        )
+
+    def test_request_reset(self):
+        response = self.client.post(
+            reverse("request_password_reset"),
+            {"email": self.user.email}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].from_email, settings.ACCOUNTS_EMAIL)
+
+        id_parser = FindHREFByID("reset_link")
+        id_parser.feed(mail.outbox[0].body)
+        url = id_parser.href
+        self.assertNotEqual(url, None, "Link was not found in email content")
+
+        query = parse_qs(urlparse(url).query)
+        self.assertIn("uid64", query.keys())
+        self.assertIn("token", query.keys())
+
+        response = self.client.post(
+            reverse("reset_password"),
+            {"uid": query["uid64"], "token": query["token"], "password": "Is this a good password?"}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password("Is this a good password?"))
