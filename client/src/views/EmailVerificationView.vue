2e272cc561bfe6c02938ec02f4532cdeaa682266
samueltl21 <samueltheodore21@gmail.com>
2025-02-15 06:30:06 +0000

    adding go to homepage link

--- client/src/views/EmailVerificationView.vue
+++ client/src/views/EmailVerificationView.vue
@@ -1,6 +1,41 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import axios from 'axios';
+import { useMessageStore } from '@/stores/message';
+import { useRouter } from 'vue-router';
+
+// use defineProps to receive the token from the router
+const props = defineProps<{ token?: string }>();
+const messageStore = useMessageStore()
+const router = useRouter();
+
+const goToHomePage = () => {
+  router.push({ path: '/' });
+}
+
+onMounted(async () => {
+  if (!props.token) {
+    messageStore.showMessage('Error', 'Invalid verification link.', 'error');
+    return;
+  }
+
+  try {
+    await axios.post('/api/verify-email', {
+      token: props.token,
+    });
+    messageStore.showMessage('Success', 'Email verified successfully', 'success');
+  } catch (error) {
+    console.error('Email verification failed:', error);
+    messageStore.showMessage('Error', 'Invalid verification link.', 'error');
+  }
+});
+
+</script>
+
 <template>
     <div class="d-flex justify-center align-center full-screen">
-    <h2>Verifying your email...</h2>
+    <h2>Verifying your email.<br></h2>
+    <a @click="goToHomePage">Go to Homepage</a>
   </div>
 </template>
 
@@ -10,28 +45,4 @@
 }
 </style>
 
-<script setup lang="ts">
-import { onMounted } from 'vue';
-import axios from 'axios';
 
-// use defineProps to receive the token from the router
-const props = defineProps<{ token?: string }>();
-
-onMounted(async () => {
-  if (!props.token) {
-    alert('Invalid verification link.');
-    return;
-  }
-
-  try {
-    await axios.post('/api/verify-email', {
-      token: props.token,
-    });
-    alert('Email verified successfully');
-  } catch (error) {
-    console.error('Email verification failed:', error);
-    alert('Email verification failed');
-  }
-});
-
-</script>
